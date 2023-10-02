4a3eda599b5a41f351e002cf50748ba71b5a3f7c
Tushar-0202-Shaw <tusharshaw987@gmail.com>
2023-10-02 17:07:59 +0530

    Identify array is sorted or not

---
+++ Array_5.java
@@ -0,0 +1,27 @@
+package College_Wallah_Array;
+import java.util.Scanner;
+public class Array_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array :-");
+        int n = sc.nextInt();
+        int[]array = new int [n];
+        System.out.println("Enter elements of array :-");
+        for(int i=0;i<n;i++){
+            array[i] = sc.nextInt();
+        }
+        boolean R = true ;
+        for(int i=0;i<n-1;i++){
+            if (array[i] > array[i+1]) {
+                R = false;
+                break;
+            }
+        }
+        if(R){
+            System.out.println("Array is sorted");
+        }
+        else{
+            System.out.println("Array is not sorted");
+        }
+    }
+}
