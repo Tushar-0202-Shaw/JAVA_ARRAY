7cc397e81940d38854ef91a4c85b290ac414e9c3
Tushar_0202_Shaw <tusharshaw987@gmail.com>
2023-11-07 10:36:01 +0530

    Max Profit in a stock

---
+++ Stock_Buy_And_Sell.java
@@ -0,0 +1,23 @@
+package College_Wallah_Array;
+import java.util.Scanner;
+public class Stock_Buy_And_Sell {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array :-");
+        int n = sc.nextInt();
+        int[]arr = new int[n];
+        System.out.println("Enter "+n+" stock :-");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((arr[j]-arr[i])>max){
+                    max = arr[j]-arr[i];
+                }
+            }
+        }
+        System.out.println("Max Profit :- "+max);
+    }
+}
